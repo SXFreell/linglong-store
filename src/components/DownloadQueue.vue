fedf0568098eb8d5a212dba1ac3edac76eeeddae
Jokul <986432015@qq.com>
2025-06-29 17:31:28 +0800

    Update DownloadQueue.vue

--- src/components/DownloadQueue.vue
+++ src/components/DownloadQueue.vue
@@ -3,18 +3,12 @@
     <transition name="el-zoom-in-left">
         <div v-show="show" class="transition-queue-box">
             <el-table :data="installingItems" border stripe style="width: 100%;height: 100%;">
-                <el-table-column label="安装进度" header-align="center" align="center" width="120" show-overflow-tooltip>
-                    <template #default="scope">
-                        <a v-if="showSchedule(scope.row)">{{ scope.row.schedule }}</a>
-                        <a v-else-if="waitingSchedule(scope.row)">等待中...</a>
-                        <a v-else>-</a>
-                    </template>
-                </el-table-column>
-                <el-table-column prop="name" label="名称" header-align="center" align="center" show-overflow-tooltip/>
-                <el-table-column prop="version" label="版本" header-align="center" align="center" width="160" show-overflow-tooltip/>
+                <el-table-column label="名称" header-align="center" align="center" :formatter="formatName" show-overflow-tooltip/>
+                <el-table-column label="版本" header-align="center" align="center" width="120" :formatter="formatVersion" show-overflow-tooltip/>
+                <el-table-column label="安装进度" header-align="center" align="center" width="90" :formatter="formatSchedule" show-overflow-tooltip/>
                 <el-table-column fixed="right" label="操作" header-align="center" align="center" width="120">
                     <template #default="scope">
-                        <el-button v-if="isInstalling(scope.row)" loading>安装中...</el-button>
+                        <el-button v-if="isInstalling(scope.row)" type="primary" size="small" loading>安装中...</el-button>
                         <el-button v-else @click="cancelInstall(scope.row)" type="danger" size="small">取消安装</el-button>
                     </template>
                 </el-table-column>
@@ -26,18 +20,11 @@
 import { PropType } from 'vue'
 import { InstalledEntity } from '@/interface'
 import { compareVersions } from '@/util/checkVersion';
+import { StopLoading } from '@/util/ReflushLoading';
 
-import { useAllAppItemsStore } from "@/store/allAppItems";
-import { useInstalledItemsStore } from "@/store/installedItems";
-import { useDifVersionItemsStore } from "@/store/difVersionItems";
-import { useUpdateItemsStore } from '@/store/updateItems';
 import { useInstallingItemsStore } from "@/store/installingItems";
 import { useSystemConfigStore } from "@/store/systemConfig";
 
-const allAppItemsStore = useAllAppItemsStore();
-const installedItemsStore = useInstalledItemsStore();
-const difVersionItemsStore = useDifVersionItemsStore();
-const updateItemsStore = useUpdateItemsStore();
 const installingItemsStore = useInstallingItemsStore();
 const systemConfigStore = useSystemConfigStore();
 
@@ -51,35 +38,39 @@ const props = defineProps({
         default: () => []
     }
 })
-// 队列表格辅助函数
-const showSchedule = (row: any) => compareVersions(systemConfigStore.linglongBinVersion, '1.5.0') >= 0 && row.schedule !== '-';
-const waitingSchedule = (row: any) => compareVersions(systemConfigStore.linglongBinVersion, '1.5.0') >= 0 && row.schedule === '-';
-const isInstalling = (row: any) => !row.isInstalled && row.loading && row.schedule !== '-';
+// 应用名称展示
+const formatName = (row: any) => row.zhName ? row.zhName : row.name;
+// 版本号展示
+const formatVersion = (row: any) => row.newVersion ? row.newVersion : row.version;
+// 安装进度展示
+const formatSchedule = (row: any) => {
+    if (compareVersions(systemConfigStore.linglongBinVersion, '1.5.0') < 0) {
+        return "-";
+    }
+    return row.schedule !== '-' ? row.schedule : '等待中...';
+}
+
+const isInstalling = (row: any) => row.schedule !== '等待中...' && row.schedule !== '-';
 
 // 终止安装点击事件
 const cancelInstall = (row: InstalledEntity) => {
-    // 关闭各个列表中的加载状态
-    allAppItemsStore.updateItemLoadingStatus(row, false);
-    installedItemsStore.updateItemLoadingStatus(row, false);
-    difVersionItemsStore.updateItemLoadingStatus(row, false);
-    updateItemsStore.updateItemLoadingStatus(row, false);
-    // 从队列中删除元素
-    installingItemsStore.removeItem(row);
+    StopLoading(row); // 关闭各个列表中的加载状态
+    installingItemsStore.removeItem(row); // 从队列中删除元素
 }
 
 </script>
 <style scoped>
 .transition-queue-box {
-    z-index: 3;
+    height: 30%;
+    width: 40%;
+    background: radial-gradient(circle at 50% 50%, transparent, var(--base-color));
+    border-radius: 12px;
     position: fixed;
     bottom: 12px;
     left: 175px;
     padding: 6px;
     box-sizing: border-box;
     text-align: center;
-    height: 28%;
-    width: 38%;
-    border-radius: 12px;
-    background: radial-gradient(circle at 50% 50%, transparent, var(--base-color));
+    z-index: 999999;
 }
 </style>
