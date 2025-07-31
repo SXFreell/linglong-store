5ce611eb280fd5db188de3f5d4301147629e1adb
Shirosu <xfreell@163.com>
2025-07-31 17:41:56 +0800

    feat: 新增网络速度监控工具，更新侧边栏组件，优化样式和功能

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,25 @@
+use serde::{Deserialize, Serialize};
+
+mod services;
+
+use services::network::{NetworkSpeed, get_network_speed as network_get_speed};
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+async fn get_network_speed() -> Result<NetworkSpeed, String> {
+    network_get_speed().await
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, get_network_speed])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
