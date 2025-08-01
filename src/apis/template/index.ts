ca69f7badf637594f2bacd59deade2f6cd181767
Shirosu <xfreell@163.com>
2025-08-01 18:27:42 +0800

    feat: 新增模板页面及相关样式和类型定义

---
+++ src/apis/template/index.ts
@@ -0,0 +1,8 @@
+// import { get, post, BaseResponse } from '../inner'
+
+// /**
+//  * template
+//  */
+// export const template = (data: any) => {
+//   return post<BaseResponse>('/template', data)
+// }
