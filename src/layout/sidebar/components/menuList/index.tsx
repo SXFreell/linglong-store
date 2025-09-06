d8c8e081b6bede09c821d1aa197887cffea362f1
Shirosu <xfreell@163.com>
2025-09-06 19:11:34 +0800

    feat: 统一样式变量，更新组件颜色和背景，新增主题样式文件

--- src/layout/sidebar/components/menuList/index.tsx
+++ src/layout/sidebar/components/menuList/index.tsx
@@ -4,22 +4,22 @@ export default [
   {
     menuName: '玲珑推荐',
     menuPath: '/',
-    icon: <Star theme="outline" size="16" fill="#4E4E4E"/>,
-    activeIcon: <Star theme="filled" size="16" fill="#016FFD"/>,
+    icon: <Star theme="outline" size="16" fill="var(--text-1)"/>,
+    activeIcon: <Star theme="filled" size="16" fill="var(--primary-6)"/>,
     index: 0,
   },
   {
     menuName: '排行榜',
     menuPath: '/ranking',
-    icon: <Ranking theme="outline" size="16" fill="#4E4E4E"/>,
-    activeIcon: <Ranking theme="filled" size="16" fill="#016FFD"/>,
+    icon: <Ranking theme="outline" size="16" fill="var(--text-1)"/>,
+    activeIcon: <Ranking theme="filled" size="16" fill="var(--primary-6)"/>,
     index: 1,
   },
   {
     menuName: '关于',
     menuPath: '/about',
-    icon: <Info theme="outline" size="16" fill="#4E4E4E"/>,
-    activeIcon: <Info theme="filled" size="16" fill="#016FFD"/>,
+    icon: <Info theme="outline" size="16" fill="var(--text-1)"/>,
+    activeIcon: <Info theme="filled" size="16" fill="var(--primary-6)"/>,
     index: 1,
   },
 ]
