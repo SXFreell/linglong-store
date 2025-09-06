e2c9437a35e810ed85300ed1e37b3cd33c481b97
caoyuee <chencaoyu5257@163.com>
2025-09-06 22:08:17 +0800

    feat:"新增基础设置页面，调整之前rem与16px换算后不为整数的问题"

--- src/layout/sidebar/components/menuList/index.tsx
+++ src/layout/sidebar/components/menuList/index.tsx
@@ -1,4 +1,4 @@
-import { Star, Ranking, Info } from '@icon-park/react'
+import { Star, Ranking, Info, SettingTwo } from '@icon-park/react'
 
 export default [
   {
@@ -16,10 +16,17 @@ export default [
     index: 1,
   },
   {
-    menuName: '关于',
+    menuName: '基础设置',
+    menuPath: '/setting',
+    icon: <SettingTwo theme="outline" size="16" fill="var(--text-1)"/>,
+    activeIcon: <SettingTwo theme="filled" size="16" fill="var(--primary-6)"/>,
+    index: 7,
+  },
+  {
+    menuName: '关于程序',
     menuPath: '/about',
     icon: <Info theme="outline" size="16" fill="var(--text-1)"/>,
     activeIcon: <Info theme="filled" size="16" fill="var(--primary-6)"/>,
-    index: 1,
+    index: 8,
   },
 ]
---
+++ src/pages/setting/index.tsx
@@ -0,0 +1,47 @@
+import { Switch } from '@arco-design/web-react'
+import styles from './index.module.scss'
+import { useState } from 'react'
+const BasicSetting = ()=>{
+  const [autoCheck, setAutoCheck] = useState(false)
+  const [showBaseService, setShowBaseService] = useState(false)
+  const [mergeAppid, setMergeAppid] = useState(false)
+  const autoCheckClick = ()=>{
+    setAutoCheck(!autoCheck)
+  }
+  const showBaseServiceClick = ()=>{
+    setShowBaseService(!showBaseService)
+  }
+  const mergeAppidClick = ()=>{
+    setMergeAppid(!mergeAppid)
+  }
+  const clearAbandonServiceClick = ()=>{
+    console.log('清除废弃基础服务')
+
+  }
+  return (
+    <div className={styles.setting}>
+      <div className={styles.basic_setting}>
+        <p className={styles.setting_name}>基础设置</p>
+        <div className={styles.setting_content}>
+          <div className={styles.content_item}>
+            <Switch size='small' checked={autoCheck} defaultChecked={autoCheck} onChange={autoCheckClick}/><span className={styles.item_label}>启动App自动检测商店版本</span>
+          </div>
+        </div>
+      </div>
+      <div className={styles.remove_setting}>
+        <p className={styles.setting_name}>卸载程序</p>
+        <div className={styles.setting_content}>
+          <div className={styles.content_item}>
+            <Switch size='small' checked={showBaseService} defaultChecked={showBaseService} onChange={showBaseServiceClick}/><span className={styles.item_label}>显示基础运行服务</span>
+          </div>
+          <div className={styles.content_item}>
+            <Switch size='small' checked={mergeAppid} defaultChecked={mergeAppid} onChange={mergeAppidClick}/><span className={styles.item_label}>同appid程序合并</span>
+          </div>
+          <p className={styles.clean_basic} onClick={clearAbandonServiceClick}>清除废弃基础服务</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default BasicSetting
