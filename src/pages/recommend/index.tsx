6c17a72c6e61835a4b729cbb190971a9c59b28ea
caoyuee <chencaoyu5257@163.com>
2025-09-09 17:46:07 +0800

    feat:"完成部分推荐页面"

--- src/pages/recommend/index.tsx
+++ src/pages/recommend/index.tsx
@@ -1,5 +1,31 @@
+import { Button } from '@arco-design/web-react'
+import AppCarousel from '@/components/ApplicationCarousel'
+import ApplicationCard from '@/components/ApplicationCard'
+import styles from './index.module.scss'
 const Recommend = () => {
-  return <div>Recommend</div>
+  return <div style={{ padding: 20 }} className={styles.recommend}>
+    <header className={styles.recommendHead}>
+      <AppCarousel />
+    </header>
+    <main className={styles.recommendMain}>
+      <div className={styles.tabBtn}>
+        {/* <button className={styles.btn}>全部应用</button> */}
+        <Button shape='round' type='default' className={styles.btn}>
+        全部应用
+        </Button>
+      </div>
+      <div className={styles.appMain}>
+        <p className={styles.name}>玲珑推荐</p>
+        <div className={styles.appList}>
+          <ApplicationCard />
+          <ApplicationCard />
+          <ApplicationCard />
+          <ApplicationCard />
+          <ApplicationCard />
+        </div>
+      </div>
+    </main>
+  </div>
 }
 
 export default Recommend
