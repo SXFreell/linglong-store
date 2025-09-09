4f7bd115aa105a8ef7d9c8a16045e43276d94799
caoyuee <chencaoyu5257@163.com>
2025-09-09 17:52:00 +0800

    feat:"优化样式，增加右边距"

--- src/pages/allApps/index.tsx
+++ src/pages/allApps/index.tsx
@@ -1,8 +1,24 @@
 import styles from './index.module.scss'
+import { Button } from '@arco-design/web-react'
 import ApplicationCard from '@/components/ApplicationCard'
 
 const AllApps = () => {
   return <div style={{ padding: 20 }}>
+    <div className={styles.tabBtn}>
+      {/* <button className={styles.btn}>全部应用</button> */}
+      <Button shape='round' type='default' className={styles.btn}>
+        全部应用
+      </Button>
+      <Button shape='round' type='default' className={styles.btn}>
+        编程开发
+      </Button>
+      <Button shape='round' type='default' className={styles.btn}>
+        视频播放
+      </Button>
+      <Button shape='round' type='default' className={styles.btn}>
+        教育学习
+      </Button>
+    </div>
     <div className={styles.applicationList}>
       <ApplicationCard />
       <ApplicationCard />
