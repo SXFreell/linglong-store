3acc9c6a900cdb64d4d9a451b128c53cd64fff74
caoyuee <chencaoyu5257@163.com>
2025-09-26 15:47:09 +0800

    feat:"新增初始化页面"

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,26 +1,41 @@
 import styles from './index.module.scss'
 
 import { Outlet } from 'react-router-dom'
-import { Suspense } from 'react'
+import { Suspense, useState, useEffect } from 'react'
 
 import Titlebar from './titlebar'
 import Sidebar from './sidebar'
+import LaunchPage from './launchPage'
 import Loading from '../components/Loading'
 
 const Layout = () => {
+  const [isInit, setIsInit] = useState(true)
+  useEffect(() => {
+    // 每次渲染后都会执行此处的代码
+    const timer = setTimeout(()=>{
+      setIsInit(false)
+    }, 5000)
+    return ()=>{
+      clearTimeout(timer)
+    }
+  })
   return (
     <div className={styles.layout}>
       <Titlebar/>
-      <div className={styles.layoutContent}>
-        <Sidebar className={styles.sider} />
-        <div className={styles.content}>
-          <div className={styles.contentInner}>
-            <Suspense fallback={<Loading />}>
-              <Outlet />
-            </Suspense>
+      {
+        isInit ? <LaunchPage /> : <div className={styles.layoutContent}>
+          <Sidebar className={styles.sider} />
+          <div className={styles.content}>
+            <div className={styles.contentInner}>
+              <Suspense fallback={<Loading />}>
+                <Outlet />
+              </Suspense>
+            </div>
           </div>
         </div>
-      </div>
+      }
+
+
     </div>
   )
 }
