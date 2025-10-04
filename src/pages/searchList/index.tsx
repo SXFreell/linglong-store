73018fe2268110ef8a14cb51aa6fc604bf9f228a
caoyuee <chencaoyu5257@163.com>
2025-10-04 10:33:07 +0800

    feat:"新增搜索回车事件"

--- src/pages/searchList/index.tsx
+++ src/pages/searchList/index.tsx
@@ -4,6 +4,7 @@ import ApplicationCard from '@/components/ApplicationCard'
 const SearchList = ()=>{
 
   return <div style={{ padding: 20 }}>
+    <p className={styles.SearchResult}>搜索结果：</p>
     <div className={styles.SearchList}>
       <ApplicationCard/>
       <ApplicationCard/>
