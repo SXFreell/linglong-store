68a3626fcf1cd60b65c11305b9e1c8abbcf82da5
caoyuee <chencaoyu5257@163.com>
2025-10-12 12:37:05 +0800

    feat:"修复样式问题"

--- src/pages/allApps/index.tsx
+++ src/pages/allApps/index.tsx
@@ -2,11 +2,22 @@ import styles from './index.module.scss'
 import { Button } from '@arco-design/web-react'
 import ApplicationCard from '@/components/ApplicationCard'
 import { useEffect, useState } from 'react'
-import { getDisCategoryList } from '@/apis/apps/index'
+import { getDisCategoryList, getSearchAppList } from '@/apis/apps/index'
+import { useInitStore } from '@/stores/appConfig'
 const AllApps = () => {
+  const arch = useInitStore((state) => state.arch)
+  const repoName = useInitStore((state) => state.repoName)
   const [categoryList, setCategoryList] = useState([])
+  const [allAppList, setAllAppList] = useState([])
+  // const customCollapseStyle = {
+  //   borderRadius: 2,
+  //   marginBottom: 24,
+  //   border: 'none',
+  //   overflow: 'hidden',
+  // }
   useEffect(()=>{
     getCategoryList()
+    getAllAppList()
   }, [])
   const getCategoryList = async()=>{
     try {
@@ -15,35 +26,59 @@ const AllApps = () => {
         setCategoryList(result.data)
       }
 
-
     } catch (error) {
       console.log(error, 'error')
     }
 
   }
-  const [activeBtn, setActiveBtn] = useState(1)
-  const handleClickBtn = (num:number)=>{
+  const getAllAppList = async(categoryId = '')=>{
+    console.log(categoryId, 'categoryId')
+
+    try {
+      const result = await getSearchAppList({ categoryId, repoName, arch, pageNo: 1, pageSize: 50 })
+      console.log(result, '所有result')
+
+      if (result.code === 200 && result.data.records.length > 0) {
+        setAllAppList(result.data.records)
+      }
+    } catch (error) {
+      console.log(error, 'error')
+    }
+  }
+  const [activeBtn, setActiveBtn] = useState<string>('')
+  const handleClickBtn = (num:string)=>{
     setActiveBtn(num)
+    getAllAppList(num)
   }
   return <div style={{ padding: 20 }}>
     <div className={styles.tabBtn}>
-      <Button shape='round' type={activeBtn === 1 ? 'primary' : 'default'} className={styles.btn} onClick={()=>handleClickBtn(1)}>
+      {/* <Collapse accordion bordered={false}>
+        <Collapse.Item header={<Button shape='round' type={activeBtn === '' ? 'primary' : 'default'} className={styles.btn} onClick={()=>handleClickBtn('')}>
+        全部应用
+        </Button>} name='1' style={customCollapseStyle}>
+          {categoryList.map(item=>{
+            return <Button shape='round' type={activeBtn === item.categoryId ? 'primary' : 'default'} key={item.id} className={styles.btn} onClick={()=>handleClickBtn(item.categoryId)}>
+              {item.categoryName}
+            </Button>
+
+          })}
+        </Collapse.Item>
+      </Collapse> */}
+      <Button shape='round' type={activeBtn === '' ? 'primary' : 'default'} className={styles.btn} onClick={()=>handleClickBtn('')}>
         全部应用
       </Button>
       {categoryList.map(item=>{
-        return <Button shape='round' type={activeBtn === item.id ? 'primary' : 'default'} key={item.id} className={styles.btn} onClick={()=>handleClickBtn(item.id)}>
+        return <Button shape='round' type={activeBtn === item.categoryId ? 'primary' : 'default'} key={item.id} className={styles.btn} onClick={()=>handleClickBtn(item.categoryId)}>
           {item.categoryName}
         </Button>
       })}
     </div>
     <div className={styles.applicationList}>
-      <ApplicationCard operateId={1}/>
-      <ApplicationCard operateId={1}/>
-      <ApplicationCard operateId={1}/>
-      <ApplicationCard operateId={1}/>
-      <ApplicationCard operateId={1}/>
-      <ApplicationCard operateId={1}/>
-      <ApplicationCard operateId={1}/>
+      {
+        allAppList.map(item=>{
+          return <ApplicationCard key={item.appId} options={item} operateId={1}/>
+        })
+      }
     </div>
   </div>
 }
